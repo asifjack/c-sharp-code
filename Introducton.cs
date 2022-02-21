a6940ba48106bdd62cad9d5ebf9731558d20702b
Md Asif <50550167+asifjack@users.noreply.github.com>
2022-02-21 01:56:21 -0800

    Add files via upload
    Intro To C#

---
+++ Introducton.cs
@@ -0,0 +1,9 @@
+What is C#
+C# is pronounced as "C-Sharp". 
+It is an object-oriented programming language provided by Microsoft that runs on .Net Framework.
+
+By the help of C# , we can develop different types applications:
+  Window applications
+  Web applications
+  Web service applications
+  Database applications etc.
